5729ebdb0d6a3ddb53a5bfefdd84121d881e299e
Medha Ghatikesh <medha.ghatikesh@sv.cmu.edu>
2012-09-26 02:54:59 -0700

    Finishing user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Beliefs::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root to: 'static_pages#home'
+
   match '/signup', to: 'users#new'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
