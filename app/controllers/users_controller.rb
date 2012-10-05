a9b85b022f63125a4bde59b92c2e6169a799840a
Medha Ghatikesh <medha.ghatikesh@sv.cmu.edu>
2012-10-05 11:49:32 -0700

    Add user following and styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+   before_filter :signed_in_user, 
+                only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
   
+  respond_to :html, :xml
 
   def show
     @user = User.find(params[:id])
     @beliefs = @user.beliefs.paginate(page: params[:page])
+
+    respond_with @user
   end
 
   def new
@@ -18,7 +22,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to 100 Beliefs!"
-      redirect_to @user
+      respond_with @user
     else
       render 'new'
     end
@@ -45,6 +49,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
+  end
+
+   def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
--- app/models/belief.rb
+++ app/models/belief.rb
@@ -2,8 +2,18 @@ class Belief < ActiveRecord::Base
   attr_accessible :content
   belongs_to :user
 
+  has_many :relationships, foreign_key: "belief_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 
   default_scope order: 'beliefs.created_at DESC'
+
+    def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", 
+          user_id: user.id)
+  end
 end
